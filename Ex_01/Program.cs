41179926eb256d4ac615115ec87de15d5ba8953b
André Felipe Sampaio Boschetti <andresampaio@id.uff.br>
2022-07-14 15:55:09 -0300

    Basic structure

--- Ex_01/Program.cs
+++ Ex_01/Program.cs
@@ -21,6 +21,32 @@
 //Não é necessário se preocupar com a adição de novas propriedades, isto é, sempre serão preenchidos apenas o Name,
 //University e RollNumber.
 //- Use Reflection para chamar o método DisplayInfo do objeto criado no item 2.1.
-//3-Ao final do exercício, coloque o seu cródigo em um repositório público no GitHub e submeta o link para avaliação.
+//3-Ao final do exercício, coloque o seu código em um repositório público no GitHub e submeta o link para avaliação.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayPublicProperties();
 
 
+        }
+        public static void DisplayPublicProperties()
+        {
+
+        }
+        public static void CreateInstance()
+        {
+
+        }
+
+    }
+}
